65f9e872d6bb1e549456bc552776e541873435c1
akh820 <122023428+akh820@users.noreply.github.com>
2023-10-23 20:50:15 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+    public class Main{
+        public static void main(String[]args){
+            Scanner scn = new Scanner(System.in);
+            	String s = scn.next();
+                System.out.println(s+"??!");//A+B, A-B, A*B, A/B(몫), A%B(나머지)
+
+                
+                
+                
+        }
+    }
