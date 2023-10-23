e440d811c5bf8e520f231dfac435afa040e57b56
akh820 <122023428+akh820@users.noreply.github.com>
2023-10-23 20:51:46 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main (String []args){
+        Scanner scn = new Scanner(System.in);
+        long A = scn.nextLong();
+        long B = scn.nextLong();
+        long C = scn.nextLong();
+
+        System.out.println(A+B+C);
+        
+    }
+}
