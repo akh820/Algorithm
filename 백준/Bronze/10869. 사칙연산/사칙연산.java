0c85dcb8732d53ef23781389fbf1f320f8b5ddb1
akh820 <122023428+akh820@users.noreply.github.com>
2023-10-23 20:50:03 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+    public class Main{
+        public static void main(String[]args){
+            Scanner scn = new Scanner(System.in);
+                int a = scn.nextInt();
+                int b = scn.nextInt();
+                System.out.println(a+b);//A+B, A-B, A*B, A/B(몫), A%B(나머지)
+                System.out.println(a-b);//A+B, A-B, A*B, A/B(몫), A%B(나머지)
+                System.out.println(a*b);//A+B, A-B, A*B, A/B(몫), A%B(나머지)
+                System.out.println(a/b);//A+B, A-B, A*B, A/B(몫), A%B(나머지)
+                System.out.println(a%b);//A+B, A-B, A*B, A/B(몫), A%B(나머지)
+                
+                
+                
+        }
+    }
