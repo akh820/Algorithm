9b63539cded0fa21264b7b640061c68bae34fecc
akh820 <122023428+akh820@users.noreply.github.com>
2023-10-23 20:50:33 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+    public class Main{
+        public static void main(String[]args){
+            Scanner scn = new Scanner(System.in);
+            	int dif = 2541 - 1998;
+            	int s = scn.nextInt();
+                System.out.println(s-dif);
+                
+                
+                
+                
+        }
+    }
