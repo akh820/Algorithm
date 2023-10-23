276fc8aa4e761c594685f07896bca21430969629
akh820 <122023428+akh820@users.noreply.github.com>
2023-10-23 20:49:37 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+    public class Main{
+        public static void main(String[]args){
+            Scanner scn = new Scanner(System.in);
+                double a = scn.nextInt();
+                double b = scn.nextInt();
+                System.out.println(a/b);//A+B, A-B, A*B, A/B(몫), A%B(나머지)
+
+                
+                
+                
+        }
+    }
