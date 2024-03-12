cec894a28f499d7dbac421d42a187ea172557d2e
akh820 <122023428+akh820@users.noreply.github.com>
2024-03-12 10:08:08 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(String message) {
+        // int answer = 0;
+        // System.out.println(message.length()*2);
+        return message.length()*2;
+    }
+}
