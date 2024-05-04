5615b44d80221904bf3adc0d64219f559a393129
akh820 <122023428+akh820@users.noreply.github.com>
2024-05-04 19:48:27 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 164 ms, Memory: 23416 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -6,42 +6,29 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int K = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        boolean[] list = new boolean[N];
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        String answer = "<";
+        Queue<Integer> q = new LinkedList<>();
 
-        int loopCount = K;
-        int i = 0;
-        int exit = 0;
-        while(exit < N){
-            exit = 0;
-            // 종료 조건
-            for(boolean b: list){
-                if(b) exit++;
-            }
-            if(!list[i]){
-                loopCount--; // 2,1,0
-                if(loopCount == 0){
-                    list[i] = true;
-                    loopCount = K;
-                    if(exit != N-1){
-                        answer += (i+1)+", ";
-                    } else {
-                        answer += (i+1)+">";
-                    }
-                }
-            }
-            i++; // 1,2,3
-            if(i == N){
-                i = 0;
-            }
+        for(int i = 1; i <= N; i++){
+            q.add(i);
         }
-        System.out.println(answer);
 
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
 
+        while (q.size() > 1){
+            for(int i = 0; i < K-1 ; i++){
+                q.offer(q.poll());
+            }
+            sb.append(q.poll()).append(", ");
+        }
+
+        sb.append(q.poll()).append(">");
+        System.out.println(sb);
     }
 }
