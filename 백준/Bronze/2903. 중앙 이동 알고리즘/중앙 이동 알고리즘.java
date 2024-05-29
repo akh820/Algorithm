4ff3556bb13e75c08b1dc626b591339ca07169e4
akh820 <122023428+akh820@users.noreply.github.com>
2024-05-29 23:55:06 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 212 ms, Memory: 17904 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        double N = scn.nextDouble(); // 반복횟수
+        double N = scn.nextDouble(); 
 
 
         double sum = 2 ;
         double tempEquation = 0;
         for ( int i = 1 ; i <= N ; i++){
-            sum += Math.pow(2.0, i-1); // sum은 3
+            sum += Math.pow(2.0, i-1);
         }
-        System.out.println((int)Math.pow(sum, 2)); // 9,
+        System.out.println((int)Math.pow(sum, 2));
 
     }
 }
