191bd20914751f6c92be830a4c95db0af6cddd48
akh820 <122023428+akh820@users.noreply.github.com>
2024-12-17 19:43:23 +0900

    [level 3] Title: 베스트앨범, Time: 10.21 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -3,42 +3,48 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         
-        Map<String,Integer> genrePlayCount = new HashMap<>();
-        Map<String,List<int[]>> genreSongs = new HashMap<>();
+        HashMap<String,Integer> sumOfPlayCount = new HashMap<>();
+        HashMap<String,List<int[]>> infoOfSongs = new HashMap<>();
         
-        for(int i = 0; i < genres.length; i++){
+        for(int i = 0; i < genres.length ; i++){
             String genre = genres[i];
             int play = plays[i];
             
-            genrePlayCount.put(genre, genrePlayCount.getOrDefault(genre, 0) + play);
-            genreSongs.computeIfAbsent(genre, key -> new ArrayList<>() ).add(new int[]{i, play});
+            sumOfPlayCount.put(genre, sumOfPlayCount.getOrDefault(genre, 0) + play);
+            
+            infoOfSongs.computeIfAbsent(genre, k -> new ArrayList<>()).add(new int[] {i, play});
+            
         }
         
-        List<String> sortedGenres = new ArrayList<>(genrePlayCount.keySet());
-        sortedGenres.sort((a,b) -> genrePlayCount.get(b) - genrePlayCount.get(a));
-        // sortedGenres.forEach(genre -> System.out.println("Genre: " + genre));
+        //분류용 값 ?
+        List<String> nameOfGenres = new ArrayList<>(sumOfPlayCount.keySet());
+        nameOfGenres.sort( (a,b) -> sumOfPlayCount.get(b) - sumOfPlayCount.get(a) );
         
         List<Integer> bestAlbum = new ArrayList<>();
-        sortedGenres.forEach( genre -> {
-           genreSongs.get(genre).stream()
-               .sorted((a,b) -> b[1] != a[1] ? b[1] - a[1] : a[0] - b[0])
-               .limit(2)
-               .forEach(song -> bestAlbum.add(song[0]));
+        
+        nameOfGenres.forEach( genre -> {
+            infoOfSongs.get(genre).stream()
+                .sorted((a,b) -> b[1] != a[1] ? b[1] - a[1] : a[0] - b[0])
+                .limit(2)
+                .forEach( e -> bestAlbum.add(e[0]));
         });
         
+        // nameOfGenres.forEach( e -> {
+        //     System.out.println("element: " + e);
+        // });
         
-        // genrePlayCount.forEach((key, value) -> {
-        //       System.out.println("key: " + key + " / value: " + value);
-        //     }
-        // );
+        // sumOfPlayCount.forEach( (key,value) -> {
+        //     System.out.println("key: " + key + " / value: " + value);
+        // });
         
-        // genreSongs.forEach((genre,songs) -> {
-        //    System.out.println("장르 : " + genre);
-        //     songs.forEach(song -> {
-        //         System.out.println("번호: " + song[0] + "/ 곡수: " + song[1]);
+        // infoOfSongs.forEach( (genre, list) -> {
+        //     System.out.println("장르: " + genre);
+        //     list.forEach( intList -> {
+        //         System.out.println("고유번호 : " + intList[0] + "/ 곡수 : " + intList[1]);
         //     });
         // });
         
-        return bestAlbum.stream().mapToInt( i -> i).toArray(); //mapToInt() Integer객체 언박싱
+        return bestAlbum.stream().mapToInt(i -> i).toArray();
+        
     }
 }
