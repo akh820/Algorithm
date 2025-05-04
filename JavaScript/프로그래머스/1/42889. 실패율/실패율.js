4067324158549f1ab2bbce98f242103e8a0d4778
akh820 <122023428+akh820@users.noreply.github.com>
2025-05-04 15:43:20 +0900

    [level 1] Title: 실패율, Time: 3935.78 ms, Memory: 43.3 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/42889. 실패율/실패율.js
@@ -0,0 +1,22 @@
+function solution(N, stages) {
+    const answer = new Array(N).fill(0);
+    let p = stages.length;
+    for(let i = 0 ; i < N ; i++){
+        let count = 0;
+        stages.filter( e => {
+            // i+1 은 스테이지 넘버
+            if(e === (i+1)) count++;
+            answer[i] = {
+                failure : count / p,
+                index : i+1
+            }
+            ;
+        })
+        p -= count;
+    }
+    let max = Math.max(...answer);
+    answer.sort((a, b) => b.failure - a.failure);
+    const realAnswer = answer.map(obj => obj.index);
+    
+    return realAnswer;
+}
