6b822f2b34977793616eb3e9eabf23bfa3a20f8b
akh820 <122023428+akh820@users.noreply.github.com>
2025-05-04 16:33:45 +0900

    [level 1] Title: 실패율, Time: 5621.23 ms, Memory: 67.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/42889. 실패율/실패율.js
+++ JavaScript/프로그래머스/1/42889. 실패율/실패율.js
@@ -1,23 +1,10 @@
 function solution(N, stages) {
-    const answer = new Array(N).fill(0);
-    let p = stages.length;
-    for(let i = 0 ; i < N ; i++){
-        let count = 0;
-        stages.filter( e => {
-            // i+1 은 스테이지 넘버
-            if(e === (i+1)) count++;
-            answer[i] = {
-                failure : count / p,
-                index : i+1
-            }
-            ;
-        })
-        p -= count;
+    let result = [];
+    for(let i=1; i<=N; i++){
+        let reach = stages.filter((x) => x >= i).length;
+        let curr = stages.filter((x) => x === i).length;
+        result.push([i, curr/reach]);
     }
-    let max = Math.max(...answer);
-    const realAnswer = answer.sort((a, b) => b.failure - a.failure)
-    .map(obj => obj.index);
-    // const realAnswer = answer.map(obj => obj.index);
-    
-    return realAnswer;
+    result.sort((a,b) => b[1] - a[1]);
+    return result.map((x) => x[0]);
 }
