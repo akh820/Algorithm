214aed0fb7b544d4ed2167ed8ba08aeda92abda8
akh820 <122023428+akh820@users.noreply.github.com>
2025-05-05 11:46:55 +0900

    [level 2] Title: 방문 길이, Time: 1.41 ms, Memory: 33.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ JavaScript/프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,34 +1,30 @@
-const isValid = function(nx, ny){
-    return nx >= -5 && nx <=5 && ny >= -5 && ny <= 5;
-}
-
-const updateLocation = function(x, y, dir){
+const isValidLocation = (nx, ny) => {
+    return nx <= 5 && nx >= -5 && ny <= 5 && ny >= -5
+} 
+      
+const updateLocation = (x, y, dir) => {
     switch(dir){
         case "U":
-            return [x, y + 1];
+            return [x, y+1];
         case "D":
-            return [x, y - 1];
+            return [x, y-1];
         case "R":
-            return [x + 1, y];
+            return [x+1, y];
         case "L":
-            return [x - 1, y];
+            return [x-1, y];
     }
 }
 
 function solution(dirs) {
     let x = 0;
     let y = 0;
-    
     const visited = new Set();
+    
     for(const dir of dirs){
-        const [nx, ny] = updateLocation(x, y, dir);
-        
-        if(!isValid(nx, ny)){
-            continue;
-        }
-        
-        visited.add(`${x}${y}${nx}${ny}`);
-        visited.add(`${nx}${ny}${x}${y}`);
+        const [nx, ny] = updateLocation(x, y ,dir);
+        if(!isValidLocation(nx, ny)) continue;
+        visited.add(`${x},${y},${nx},${ny}`);
+        visited.add(`${nx},${ny},${x},${y}`);
         [x, y] = [nx, ny];
     }
     
