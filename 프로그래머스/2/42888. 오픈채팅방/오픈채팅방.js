1072c2336a76ed58cc4e5ddb41d317b857f01a8a
akh820 <122023428+akh820@users.noreply.github.com>
2025-05-22 10:22:38 +0900

    [level 2] Title: 오픈채팅방, Time: 305.95 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,63 +1,23 @@
-class Queue{
-    constructor(){
-        this.storage = {};
-        this.rear = 0;
-        this.front = 0;
-    }
-    
-    enqueue(val){
-        this.storage[this.rear++] = val;
-    }
-    
-    dequeue(){
-        if (this.size() === 0) return "큐가 비어있음";
-        const val = this.storage[this.front];
-        delete this.storage[this.front++];
-        return val
-    }
-    
-    size(){
-        return this.rear - this.front;
-    }
-}
-
-function solution(record) {
+function solution(record){
     const answer = [];
-    const nameObj = {};
-    const queue = new Queue();
+    const uid = {};
     
-    for(const r of record){
-        const infos = r.split(" ");
-        if(!nameObj[infos[1]]){
-            nameObj[infos[1]] = infos[2];
-            queue.enqueue([infos[0], infos[1]]);
-        } else {
-            switch (infos[0]) {
-                case "Enter":
-                    if (nameObj[infos[1]] !== infos[2]) {
-                        nameObj[infos[1]] = infos[2];
-                    }
-                    queue.enqueue([infos[0], infos[1]]);
-                    break;
-                case "Leave":
-                    queue.enqueue([infos[0], infos[1]]);
-                    break;
-                case "Change":
-                    nameObj[infos[1]] = infos[2];
-                    break;
-            }
+    for(const line of record){
+        const div = line.split(" ");
+        //1: 명령어, 2:id, 3:닉네임
+        if(div[0] != "Leave"){
+            uid[div[1]] = div[2];
         }
     }
-    // console.log(queue);
-    // console.log(nameObj);
-    const n = queue.size();
-    for(let i = 0 ; i < n; i++){
-        const value = queue.dequeue();
-        if(value[0] === "Enter"){
-            answer.push(`${nameObj[value[1]]}님이 들어왔습니다.`);
-        } else if (value[0] === "Leave"){
-            answer.push(`${nameObj[value[1]]}님이 나갔습니다.`);
+    
+    for(const line of record){
+        const div = line.split(" ");
+        if(div[0] === "Enter"){
+            answer.push(`${uid[div[1]]}님이 들어왔습니다.`);
+        } else if(div[0] === "Leave"){
+            answer.push(`${uid[div[1]]}님이 나갔습니다.`);
         }
     }
+    
     return answer;
 }
