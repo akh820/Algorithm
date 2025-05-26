9eaf02fea20ff68955bf3c02c54dd3019d7ebaf2
akh820 <122023428+akh820@users.noreply.github.com>
2025-05-26 10:20:46 +0900

    [level 3] Title: 베스트앨범, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.38 ms
+메모리: 33.5 MB, 시간: 0.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 23일 15:16:08
+2025년 05월 26일 10:20:43
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,45 +1,38 @@
 function solution(genres, plays) {
-    const genresObj = {};
-    const sumObj = {};
-    const answer = [];
+  const answer = [];
+  const genresObj = {};
+  const playObj = {};
 
-    // 1. 장르별 곡 정보와 장르별 총 재생 수 저장
-    for (let i = 0; i < genres.length; i++) {
-        const genre = genres[i];
-        const play = plays[i];
-        // 곡 정보: [곡 인덱스, 재생 수]
-        if (genresObj[genre]) {
-            genresObj[genre].push([i, play]);
-        } else {
-            genresObj[genre] = [[i, play]];
-        }
-        // 장르별 총 재생 수
-        if (sumObj[genre]) {
-            sumObj[genre] += play;
-        } else {
-            sumObj[genre] = play;
-        }
+  for (let i = 0; i < genres.length; i++) {
+    const genre = genres[i];
+    const play = plays[i];
+      
+    // if(!genresObj[genre]){
+    //     genresObj[genre] = [[i,play]];
+    //     playObj[genre] = play;
+    // } else {
+    //     genresObj[genre].push([i,play]);
+    //     playObj[genre] == play;
+    // }
+
+    if (!(genre in genresObj)) {
+      genresObj[genre] = [];
+      playObj[genre] = 0;
     }
 
-    // 2. 각 장르별로 곡 리스트를 재생 수 내림차순, 인덱스 오름차순으로 정렬
-    const sortedGenresObj = Object.fromEntries(
-        Object.entries(genresObj).map(([key, arr]) => [
-            key,
-            arr.sort((a, b) => b[1] === a[1] ? a[0] - b[0] : b[1] - a[1])
-        ])
-    );
+    genresObj[genre].push([i, play]);
+    playObj[genre] += play;
+  }
 
-    // 3. 장르별 총 재생 수 내림차순 정렬
-    const sortedEntries = Object.entries(sumObj).sort((a, b) => b[1] - a[1]);
+  const sortedGenres = Object.keys(playObj).sort((a, b) => playObj[b] - playObj[a]);
 
-    // 4. 장르별로 최대 2곡씩 answer에 추가
-    for (const [genre] of sortedEntries) {
-        const songs = sortedGenresObj[genre];
-        // 최대 2곡까지
-        for (let i = 0; i < Math.min(2, songs.length); i++) {
-            answer.push(songs[i][0]);
-        }
-    }
+  for (const genre of sortedGenres) {
+    const sortedSongs = genresObj[genre].sort((a, b) => {
+      return a[1] === b[1] ? a[0] - b[0] : b[1] - a[1];
+    });
 
-    return answer;
+    answer.push(...sortedSongs.slice(0, 2).map(song => song[0]));
+  }
+
+  return answer;
 }
