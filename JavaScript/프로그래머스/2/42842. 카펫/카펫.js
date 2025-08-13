aebf10ab4990077c062ce5a990e1572cf2de5723
안계홍 <122023428+akh820@users.noreply.github.com>
2025-08-13 16:45:19 +0900

    [level 2] Title: 카펫, Time: 0.29 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/42842. 카펫/카펫.js
@@ -0,0 +1,16 @@
+//width , length, volume;
+
+function solution(brown, yellow) {
+    const NumberOfTiles = brown + yellow;
+    for(let i = 1; i <= NumberOfTiles; i++){
+        let width = 0;
+        let length = 0;
+        if(Math.floor((NumberOfTiles / i)) * i === NumberOfTiles &&
+          Math.floor((NumberOfTiles / i)) >= i) {
+            width = (NumberOfTiles / i);
+            length = i;
+            if((width - 2) * (length - 2) === yellow) return [width, length];
+        }
+    }
+    return NumberOfTiles;
+}
