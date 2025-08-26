de14ab9b91fbc8344a06d0aa85ba0a6a794ef379
안계홍 <122023428+akh820@users.noreply.github.com>
2025-08-26 23:12:57 +0900

    [level 2] Title: 큰 수 만들기, Time: 40.91 ms, Memory: 49.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
+++ JavaScript/프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,14 +1,18 @@
 function solution(number, k){
-    const stack = [];
-        for(let i = 0 ; i < number.length; i++){
-            //11999
-            while(stack.length > 0 && stack[stack.length - 1] < number[i] && k > 0){
-                stack.pop();
-                k--;
-            } // 9
-            stack.push(number[i]); // 11
-        }
-    
-    stack.splice(stack.length - 1, k);
-    return stack.join("");
+
+  const stack = [];
+  let i = 0;
+  
+  while(i < number.length){
+    while(stack[stack.length-1] < number[i] && k > 0){
+      stack.pop();
+      k--;
+    }
+    stack.push(number[i]);
+    i++;
+  }
+    stack.splice(stack.length-k, k);
+
+
+  return stack.join("");
 }
