6477f8ead466a91c8a15fa9664fdfd3d5a231bf5
안계홍 <122023428+akh820@users.noreply.github.com>
2025-09-01 22:06:58 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ JavaScript/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,38 +1,4 @@
 function solution(n, arr1, arr2) {
-    const result = [];
-    const result1 = [];
-    const result2 = [];
-    for(const e of arr1){
-        result1.push(decimalToBinary(e,n));
-    }
-    for(const e of arr2){
-        result2.push(decimalToBinary(e,n));
-    }
-    for(let i = 0 ; i < result1.length; i++){
-        let string = "";
-        for(let j = 0 ; j < result1[i].length; j++){
-            if(result1[i][j] === "#" || result2[i][j] === "#"){
-                string += "#";
-            } else {
-                string += " ";
-            }
-        }
-        result.push(string);
-    }
-    
-    function decimalToBinary(decimal, exponent){
-        let string = "";
-        while(exponent > 0){
-            if(decimal >= Math.pow(2,exponent-1) && decimal < Math.pow(2,exponent)){
-                decimal -= Math.pow(2,exponent-1);
-                string += "#";
-            } else {
-                string += " ";
-            }
-            exponent--;
-        }
-        return string;
-    }
-    
-    return result;
+    return arr1.map((e,i) => ((e | arr2[i]).toString(2).padStart(n,"0")
+                              .replaceAll("0"," ").replaceAll("1","#")))
 }
