50ae7312648a1188bf77462c90962fc606781129
안계홍 <122023428+akh820@users.noreply.github.com>
2025-09-03 00:38:18 +0900

    [level 2] Title: 조이스틱, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42860. 조이스틱/조이스틱.js
+++ JavaScript/프로그래머스/2/42860. 조이스틱/조이스틱.js
@@ -1,28 +1,30 @@
 function solution(name) {
-  let totalCount = 0;
-  for(const e of name){
-    totalCount += upDown(e);
-  }
-
-  let minMove = name.length-1;
-
-  for(let i = 0 ; i < name.length; i++){
-    let nextIndex = i + 1;
-    while(nextIndex < name.length && name[nextIndex] === "A"){
-      nextIndex++;
+    
+    let upDownCount = 0;
+    
+    for(const e of name){
+        upDownCount += upDown(e);
     }
-
-    const goRightThenTurnLeft = ( i * 2) + (name.length - nextIndex);
-    const goLeftThenTurnRight = ( (name.length - nextIndex) * 2) + i;
-
-    minMove = Math.min(minMove, goRightThenTurnLeft, goLeftThenTurnRight);
-
-  }
-
-  return totalCount + minMove;
+    
+    let minMove = name.length - 1;
+    for(let i = 0 ; i < name.length; i++){
+        let nextIndex = i + 1;
+        while(nextIndex < name.length && name[nextIndex] === "A"){
+            nextIndex++;
+        }
+        
+        const goRightThenTurn = (i * 2) + (name.length - nextIndex);
+        const goLeftThenTurn = (name.length - nextIndex) * 2 + i;
+        
+        minMove = Math.min(goRightThenTurn, goLeftThenTurn, minMove);
+    }
+    
+    
+    function upDown(alphabet){
+        const code = alphabet.charCodeAt(0);
+        return Math.min(code - 65, 91 - code);
+    }
+        
+    return upDownCount + minMove;
+    
 }
-
-function upDown(alphabet){
-    const value = alphabet.charCodeAt(0);
-    return Math.min(value - 65, 91 - value);
-  }
