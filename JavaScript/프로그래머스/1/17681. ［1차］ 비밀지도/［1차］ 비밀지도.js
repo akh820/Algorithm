07a3cfea2060e2e0a010afa17a68d38de1eec28b
안계홍 <122023428+akh820@users.noreply.github.com>
2025-09-02 23:49:29 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ JavaScript/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,4 +1,3 @@
 function solution(n, arr1, arr2) {
-    return arr1.map((e,i) => ((e | arr2[i]).toString(2).padStart(n,"0")
-                              .replaceAll("0"," ").replaceAll("1","#")))
+    return arr1.map((e,i) => ( e | arr2[i] ).toString(2).padStart(n, "0").replaceAll("1","#").replaceAll("0", " "))
 }
