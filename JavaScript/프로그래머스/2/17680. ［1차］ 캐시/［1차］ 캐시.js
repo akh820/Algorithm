6bc962327ae1ea7f1dfae80dac63820d667aab17
안계홍 <122023428+akh820@users.noreply.github.com>
2025-09-02 21:46:17 +0900

    [level 2] Title: [1차] 캐시, Time: 27.95 ms, Memory: 42.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ JavaScript/프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,45 +1,25 @@
 function solution(cacheSize, cities) {
-    let count = 0;
-    const cacheMap = new Map();
-    cities.forEach((e,i) => {
-        if(cacheSize === 0){
-            count += 5;
-        } else {
-            const city = e.toLowerCase();
-            if(cacheMap.size < cacheSize){
-                if(cacheMap.has(city)){
-                    count += 1;
-                } else {
-                    count += 5;
-                }
-                cacheMap.set(city, 0);
-            } else {
-                if(cacheMap.has(city)){ // 기존꺼를 0으로
-                    count += 1;
-                } else { // 기존에 가장 시간이 오래 경과된놈을 지움
-                    const key = findOldestCity();
-                    cacheMap.delete(key);
-                    count += 5;
-                }
-                cacheMap.set(city, 0);
-            }
-            cacheMap.forEach((value, key) => {
-                cacheMap.set(key, value+1);
-            })
-        }
-    })
+    if(cacheSize === 0){
+        return cities.length * 5;
+    }
     
-    function findOldestCity(){
-        let oldest = 0;
-        let key;
-        for(const [k, v] of cacheMap.entries()){
-            if(v > oldest){
-                key = k
-                oldest = v
+    let count = 0;
+    const cache = new Map();
+    
+    for(const city of cities){
+        const cityLower = city.toLowerCase();
+        if(cache.has(cityLower)){
+            count += 1;
+            cache.delete(cityLower);
+            cache.set(cityLower, true); // value는 상관없음
+        } else {
+            count += 5;
+            if(cache.size >= cacheSize){
+                const oldestKey = cache.keys().next().value;
+                cache.delete(oldestKey);
             }
+            cache.set(cityLower, true);
         }
-        
-        return key
     }
 
     return count;
