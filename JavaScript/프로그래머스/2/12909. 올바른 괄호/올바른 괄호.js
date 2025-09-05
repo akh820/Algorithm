13c4e57f1241c1013ee9d5cfce177a4c224b8c41
안계홍 <122023428+akh820@users.noreply.github.com>
2025-09-05 16:41:45 +0900

    [level 2] Title: 올바른 괄호, Time: 4.40 ms, Memory: 37.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ JavaScript/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,17 +1,31 @@
-function solution (s) {
-    const stack = [];
-    for(const e of s){
-        const peek = stack[stack.length-1];
-        if(stack.length === 0){
-            if(e === '(') stack.push(e);
-            else if (e === ')') return false;
-        } else {
-            if(e === ')'){
-                if(peek === '('){
-                    stack.pop();
-                } else if (peek === ')') stack.push(e);
-            } else if ( e === '(') stack.push(e);
-        }
-    }
-    return stack.length === 0 ? true : false;
+function solution(s) {
+    // let count = 0;
+    // for(const e of s){
+    //     if(e === '('){
+    //         count++;
+    //     } else {
+    //         if(count === 0) return false;
+    //         count--;
+    //     }
+    // }
+    // return count === 0;
+    return correctBracket(s);
 }
+
+function correctBracket(string){
+        const stack = [];
+        for(let i = 0 ; i < string.length; i++){
+            const bracket = string[i];
+            if(bracket === "("){
+                stack.push(bracket);
+            } else { // ")"
+                if(stack.length === 0){
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.length === 0;
+    }
+
