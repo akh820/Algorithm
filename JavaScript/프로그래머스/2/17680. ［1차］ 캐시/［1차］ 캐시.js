e15241f8e535691edb9d3cc5000a538ddd8d4d91
안계홍 <122023428+akh820@users.noreply.github.com>
2025-09-09 20:23:59 +0900

    [level 2] Title: [1차] 캐시, Time: 29.13 ms, Memory: 43.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ JavaScript/프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,31 +1,31 @@
 function solution(cacheSize, cities) {
-    
+    if(cacheSize === 0) return cities.length * 5;
     let time = 0;
-    
-    if(cacheSize === 0){
-        return cities.length * 5;
-    }
-    
     const cache = new Map();
-    
-    for(const e of cities){
-        const city = e.toLowerCase();
-        if(cache.has(city)){
-            cache.delete(city);
-            cache.set(city, true);
-            time += 1;
-        } else {
-            if(cache.size < cacheSize){
+    cities.map((e) => {
+        const city = e.toUpperCase();
+        if(cache.size < cacheSize){ // cache.size가 주어진 크기보다 작을때
+            if(cache.has(city)){
+                cache.delete(city);
                 cache.set(city, true);
+                time += 1;
             } else {
-                const oldestKey = cache.keys().next().value;
-                cache.delete(oldestKey);
                 cache.set(city, true);
+                time += 5;
             }
-            time += 5;
+        } else {
+            if(cache.has(city)){
+                cache.delete(city);
+                cache.set(city, true);
+                time += 1;
+            } else {
+                const key = cache.keys().next().value;
+                cache.delete(key);
+                cache.set(city, true);
+                time += 5;
+            }
         }
-    }
-    
+    })
     return time;
 }
 
