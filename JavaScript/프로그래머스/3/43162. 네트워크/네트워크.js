d184b7c4d5878b2290327688554fb5d6cc2934cf
안계홍 <122023428+akh820@users.noreply.github.com>
2025-09-12 12:28:15 +0900

    [level 3] Title: 네트워크, Time: 6.60 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/3/43162. 네트워크/네트워크.js
@@ -0,0 +1,24 @@
+function solution(n, computers) {
+    const set = new Set();
+    computers.forEach((computer,i) => {
+        const visited = new Array(n).fill(false);
+        set.add(check(computer, i, visited).join(""));
+    })
+    
+    function check(arr, i, visited){ // arr 가 [1,1,0] 이라고 가정
+        for(let j = 0 ; j < arr.length; j++){
+            if(j === i){ // 같으면 무조건 1
+                visited[j] = true;
+            }
+            if(arr[j] === 1 && visited[j] === false){
+                visited[j] = true;
+                check(computers[j],i,visited);   //j가 1일때, check([1,1,0],0,[t,t,f])
+            }
+            // console.log("i =",i, "j =", j, visited);
+        }
+        // console.log(set);
+        return visited
+    }
+    
+    return set.size;
+}
