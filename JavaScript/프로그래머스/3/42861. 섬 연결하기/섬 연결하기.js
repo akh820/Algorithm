fc25ef92f60ec67dba07ab545ec09a55d90184a0
안계홍 <122023428+akh820@users.noreply.github.com>
2025-09-17 00:16:07 +0900

    [level 3] Title: 섬 연결하기, Time: 0.46 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
+++ JavaScript/프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
@@ -1,34 +1,35 @@
 function solution(n, costs) {
+    const rootNodes = Array.from({length : n}, (_,i) => i);
+    let minCost = 0;
+    
     costs.sort((a,b) => a[2] - b[2]);
     
-    const rootNodes = Array.from({length : n}, (_,i) => i);
-    
-    //[[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]
-    // 3의 부모 2
-    // 2의 부모 1
-    // 1의 부모 0
-    const findRootNode = (i) => {
-        if(rootNodes[i] === i) {
-            return i;
-        }
-        return rootNodes[i] = findRootNode(rootNodes[i]);
-    }
-    
-    const unionRoots = (a,b) => {
-        const rootA = findRootNode(a); // 0
-        const rootB = findRootNode(b); // 1
-        
-        if(rootA !== rootB){
-            rootNodes[rootB] = rootA;
-            return true;
-        }
-        return false;
-    }
-    let minCost = 0;
-    for(const [a, b, cost] of costs){
+    for(const [a,b, cost] of costs){
         if(unionRoots(a,b)){
             minCost += cost;
         }
     }
+    
+    function findRootNode(e){
+        if(rootNodes[e] === e){
+            return e;
+        }
+        return rootNodes[e] = findRootNode(rootNodes[e]);
+        
+    }
+    
+    function unionRoots(a, b){
+        const rootA = findRootNode(a);
+        const rootB = findRootNode(b);
+
+        if(rootA !== rootB){
+            rootNodes[rootB] = rootA;
+            return true;
+        }
+
+        return false;
+    }
+    
     return minCost;
 }
+
