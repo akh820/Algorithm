d25b6dab56cd5e5d4fa40c7cfb74af0256103931
안계홍 <122023428+akh820@users.noreply.github.com>
2025-09-16 23:26:02 +0900

    [level 2] Title: 소수 찾기, Time: 8.77 ms, Memory: 36.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ JavaScript/프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,34 +1,37 @@
-function solution (numbers) {
-    const numberList = numbers.split("");
-    const uniqueNumber = new Set();
-    let primeCount = 0;
+function solution(numbers) {
+    const set = new Set();
+    let count = 0;
     
-    function isPrime(num) {
-        if(num < 2) return false;
-        for(let i = 2; i <= Math.sqrt(num) ; i++){
-            if(num % i === 0) return false;
-        }
-        return true;
-    }
+    newCombination(numbers.split(""), "");
     
-    function createNumberCombination(arr, prefix){
-        if(prefix.length > 0){
-            uniqueNumber.add(Number(prefix));
+    function newCombination(arr, prefix){
+        if(prefix.length > 0){ 
+            set.add(Number(prefix)); // set = 1
         }
         
-        for(let i  = 0 ; i < arr.length; i++){
-            const newPrefix = prefix + arr[i];
-            const remainingNumber = [...arr];
-            remainingNumber.splice(i,1);
-            createNumberCombination(remainingNumber, newPrefix);
+        for(let i = 0 ; i < arr.length; i++){ // [1,7] 에서 0시작
+                                              // [7] 에서 0시작
+            const newPrefix = arr[i] + prefix; // 1 + "" // 7 + "1"
+            const remaining = [...arr]; // [1,7]; // 7
+            remaining.splice(i,1); // [7] // 0
+            newCombination(remaining, newPrefix); // [7], "71";
         }
     }
     
-    createNumberCombination(numberList, "");
-    
-    for(const e of uniqueNumber){
-        if(isPrime(e)) primeCount++;
+    for(const e of set){
+        if(isPrime(e)){
+            count++;
+        }
     }
     
-    return primeCount;
+    return count;
+}
+
+
+function isPrime(number){
+    if(number < 2) return false;
+    for(let i = 2; i <= Math.sqrt(number); i++){
+        if(number % i === 0) return false;
+    }
+    return true;
 }
