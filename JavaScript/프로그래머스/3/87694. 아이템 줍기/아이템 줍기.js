cdd771c84b698f611621e0a3b0f1729c420ee645
안계홍 <122023428+akh820@users.noreply.github.com>
2025-09-22 10:19:08 +0900

    [level 3] Title: 아이템 줍기, Time: 2.90 ms, Memory: 34 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/87694. 아이템 줍기/아이템 줍기.js
+++ JavaScript/프로그래머스/3/87694. 아이템 줍기/아이템 줍기.js
@@ -1,70 +1,67 @@
 function solution(rectangle, characterX, characterY, itemX, itemY) {
-    
-    const expandedSquares = rectangle.map( rec => {
-        const result = [];
-        for(const e of rec){
-            result.push(e * 2);
+    const extandedRecs = rectangle.map(e => {
+        const arr = [];
+        for(let i = 0 ; i < e.length; i++){
+            arr.push(e[i] * 2);
         }
-        return result;
-    })
+        return arr; 
+    });
     
-    const linePoint = new Set();
-    for(const [x1,y1,x2,y2] of expandedSquares){
-        for(let i = x1 ; i <= x2; i++){
-            linePoint.add(`${i},${y1}`);
-            linePoint.add(`${i},${y2}`);
+    const set = new Set();
+    
+    for( const [x1,y1,x2,y2] of extandedRecs ){
+        for(let i = x1; i <= x2; i++){
+            set.add(`${i},${y1}`);
+            set.add(`${i},${y2}`);
         }
-        for(let i = y1 ; i <= y2; i++){
-            linePoint.add(`${x1},${i}`);
-            linePoint.add(`${x2},${i}`);
+        for(let i = y1; i <= y2; i++){
+            set.add(`${x1},${i}`);
+            set.add(`${x2},${i}`);
         }
     }
     
-    const outlinePoint = new Set();
-    for(const e of linePoint){
-        const [x, y] = e.split(',').map(Number);
-        let isInside = false;
-        for(const [x1,y1,x2,y2] of expandedSquares){
+    const newSet = new Set();
+    for( const e of set ){
+        const [x, y] = e.split(",").map(Number);
+        let isInBoundary = false;
+        
+        for( const [x1,y1,x2,y2] of extandedRecs ){
             if(x > x1 && x < x2 && y > y1 && y < y2){
-                isInside = true;
+                isInBoundary = true;
                 break;
             }
         }
-        if(isInside === false){
-            outlinePoint.add(`${x},${y}`);
+        if(!isInBoundary){
+            newSet.add(`${x},${y}`);
         }
     }
-        
-    const visited = new Map();
-    for(const e of outlinePoint){
-        visited.set(e, false);
+    const visited = {};
+    for(const e of newSet){
+        visited[e] = false;
     }
         
-    const queue = [[characterX * 2, characterY * 2, 0]];
-    visited.set(`${characterX * 2},${characterY * 2}`,true);
-    
     const dx = [-1,1,0,0];
     const dy = [0,0,-1,1];
-        
+    
+    const queue = [[characterX * 2 , characterY * 2 , 0]];
+    visited[`${characterX * 2},${characterY * 2}`] = true;
+    
     while(queue.length > 0){
-        
         const [x, y, distance] = queue.shift();
-        if( x === itemX * 2 && y === itemY * 2){
+        if(x === itemX * 2 && y === itemY * 2){
             return distance / 2;
         }
         
-        for(let i = 0 ; i < 4; i++){
-            
+        for(let i = 0 ; i < 4 ; i++){
             const nx = dx[i] + x;
             const ny = dy[i] + y;
-            const nextPoint = `${nx},${ny}`;
+            const checkString = `${nx},${ny}`;
             
-            if(visited.has(nextPoint) && visited.get(nextPoint) === false){
-                visited.set(nextPoint, true);
-                queue.push([nx,ny, distance + 1]);
+            if(visited[checkString] === false){
+                visited[checkString] = true;
+                queue.push([nx, ny, distance + 1]);
             }
         }
     }
-        
     return -1;
 }
