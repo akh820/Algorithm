8016658070f201e9d94d1f81e816835606aea509
안계홍 <122023428+akh820@users.noreply.github.com>
2025-09-26 17:07:35 +0900

    [level 2] Title: 조이스틱, Time: 0.35 ms, Memory: 33.3 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42860. 조이스틱/조이스틱.js
+++ JavaScript/프로그래머스/2/42860. 조이스틱/조이스틱.js
@@ -1,26 +1,26 @@
-function solution(name){
+function solution(name) {
+    
+    function upDown(alphabet){
+        const code = alphabet.charCodeAt(0);
+        return Math.min(91 - code, code - 65);
+    }
+    
     let upDownCount = 0;
     for(const e of name){
         upDownCount += upDown(e);
     }
-    
-    let leftRightMin = name.length;
+    let smallestCount = name.length - 1;
     
     for(let i = 0 ; i < name.length; i++){
-        let currentIndex = i + 1;
-        while(currentIndex < name.length && name[currentIndex] === "A"){
-            currentIndex++;
+        let nextIndex = i + 1;
+        while(nextIndex < name.length && name[nextIndex] === "A"){
+            nextIndex++;
         }
-                
-        const goRightThenTurnLeft = (i * 2) + (name.length - currentIndex);
-        const goLeftThenTurnRight = (name.length - currentIndex) * 2 + i;
-        leftRightMin = Math.min(goRightThenTurnLeft,goLeftThenTurnRight,leftRightMin);
+        const goRightThenTurnLeft = (i * 2) + (name.length - nextIndex);
+        const goLeftThenTurnRight = (name.length - nextIndex) * 2 + i;
+        
+        smallestCount = Math.min(smallestCount, goRightThenTurnLeft, goLeftThenTurnRight);
     }
     
-    return upDownCount + leftRightMin;
-}
-
-function upDown(alphabet){
-    const code = alphabet.charCodeAt(0);
-    return Math.min(code - 65, 91 - code);
+    return smallestCount + upDownCount;
 }
