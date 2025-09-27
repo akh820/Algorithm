e12272395ce052d943cf7742fe8cf63c7cd46b5c
안계홍 <122023428+akh820@users.noreply.github.com>
2025-09-27 21:21:58 +0900

    [level 3] Title: 아이템 줍기, Time: 2.33 ms, Memory: 33.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/87694. 아이템 줍기/아이템 줍기.js
+++ JavaScript/프로그래머스/3/87694. 아이템 줍기/아이템 줍기.js
@@ -1,67 +1,60 @@
 function solution(rectangle, characterX, characterY, itemX, itemY) {
-    const extandedRecs = rectangle.map(e => {
-        const arr = [];
-        for(let i = 0 ; i < e.length; i++){
-            arr.push(e[i] * 2);
-        }
-        return arr; 
-    });
+    //2배 늘리기
+    const extandedRects = rectangle.map(([x1,y1,x2,y2]) => [x1*2,y1*2,x2*2,y2*2]);
     
-    const set = new Set();
-    
-    for( const [x1,y1,x2,y2] of extandedRecs ){
+    const beforeSet = new Set();
+    for(const [x1,y1,x2,y2] of extandedRects){
         for(let i = x1; i <= x2; i++){
-            set.add(`${i},${y1}`);
-            set.add(`${i},${y2}`);
+            beforeSet.add(`${i},${y1}`);
+            beforeSet.add(`${i},${y2}`);
         }
         for(let i = y1; i <= y2; i++){
-            set.add(`${x1},${i}`);
-            set.add(`${x2},${i}`);
+            beforeSet.add(`${x1},${i}`);
+            beforeSet.add(`${x2},${i}`);
         }
     }
-    
-    const newSet = new Set();
-    for( const e of set ){
+    const afterSet = new Set();
+    for(const e of beforeSet){
         const [x, y] = e.split(",").map(Number);
-        let isInBoundary = false;
-        
-        for( const [x1,y1,x2,y2] of extandedRecs ){
+        let inSide = false;
+        for(const [x1,y1,x2,y2] of extandedRects){
             if(x > x1 && x < x2 && y > y1 && y < y2){
-                isInBoundary = true;
+                inSide = true;
                 break;
             }
         }
-        if(!isInBoundary){
-            newSet.add(`${x},${y}`);
+        if(inSide === false){
+            afterSet.add(`${x},${y}`);
         }
     }
-    const visited = {};
-    for(const e of newSet){
-        visited[e] = false;
-    }
-        
+    
     const dx = [-1,1,0,0];
     const dy = [0,0,-1,1];
     
-    const queue = [[characterX * 2 , characterY * 2 , 0]];
-    visited[`${characterX * 2},${characterY * 2}`] = true;
+    const queue = [[characterX*2, characterY*2,0]];
+    
+    const visited = {};
+    for(const e of afterSet){
+        visited[e] = false;
+    }
+    visited[`${characterX*2},${characterY*2}`] = true;
     
     while(queue.length > 0){
-        const [x, y, distance] = queue.shift();
+        const [x,y,distance] = queue.shift();
         if(x === itemX * 2 && y === itemY * 2){
             return distance / 2;
         }
         
-        for(let i = 0 ; i < 4 ; i++){
+        for(let i = 0 ; i < 4; i ++){
             const nx = dx[i] + x;
             const ny = dy[i] + y;
-            const checkString = `${nx},${ny}`;
-            
-            if(visited[checkString] === false){
-                visited[checkString] = true;
-                queue.push([nx, ny, distance + 1]);
+            const nextPoint = `${nx},${ny}`;
+            if(visited[nextPoint] === false){
+                visited[nextPoint] = true;
+                queue.push([nx, ny, distance+1]);
             }
         }
     }
+    
     return -1;
 }
