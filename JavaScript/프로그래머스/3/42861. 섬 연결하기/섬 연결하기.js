1e5f51d4bc99f7b38400d549817e0c8351658736
안계홍 <122023428+akh820@users.noreply.github.com>
2025-09-29 17:56:35 +0900

    [level 3] Title: 섬 연결하기, Time: 17.18 ms, Memory: 37.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
+++ JavaScript/프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
@@ -1,35 +1,32 @@
 function solution(n, costs) {
-    const rootNodes = Array.from({length : n}, (_,i) => i);
-    let minCost = 0;
+    const rootNodeList = Array.from({length:n}, (_,i)=>i);
     
     costs.sort((a,b) => a[2] - b[2]);
     
-    for(const [a,b, cost] of costs){
-        if(unionRoots(a,b)){
-            minCost += cost;
+    let cost = 0;
+    
+    for(const [a, b, distance] of costs){
+        console.log(rootNodeList, "Before");
+        if(unionRoot(a,b)){
+            cost += distance;
         }
+        console.log(rootNodeList, "After");
     }
     
-    function findRootNode(e){
-        if(rootNodes[e] === e){
-            return e;
-        }
-        return rootNodes[e] = findRootNode(rootNodes[e]);
-        
-    }
-    
-    function unionRoots(a, b){
+    function unionRoot(a, b){
         const rootA = findRootNode(a);
         const rootB = findRootNode(b);
-
         if(rootA !== rootB){
-            rootNodes[rootB] = rootA;
+            rootNodeList[rootB] = rootA;
             return true;
         }
-
-        return false;
     }
-    
-    return minCost;
-}
+    function findRootNode(e){
+        if(rootNodeList[e] === e){
+            return e;
+        }
+        return rootNodeList[e] = findRootNode(rootNodeList[e]);
+    }
 
+    return cost;
+}
