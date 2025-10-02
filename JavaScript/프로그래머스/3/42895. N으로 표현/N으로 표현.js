d816ba33de269709daaca658b68cfe5b22d4c19d
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-02 11:50:53 +0900

    [level 3] Title: N으로 표현, Time: 44.46 ms, Memory: 39.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/42895. N으로 표현/N으로 표현.js
+++ JavaScript/프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -1,14 +1,15 @@
 function solution(N, number) {
-    const dp = Array.from({length:9}, () => new Set());
-    for(let i = 1; i < 9 ; i++){
+    const dp = Array.from({length : 9} , () => new Set());
+    
+    for(let i = 1 ; i < 9; i++){
         dp[i].add(Number(String(N).repeat(i)));
-        for(let j = 1; j < i; j++){
-            const k = i - j;
+        for(let j = 1 ; j < i; j++){
+            const k = i - j ;
             for(const num1 of dp[j]){
                 for(const num2 of dp[k]){
+                    dp[i].add(num1 * num2);
                     dp[i].add(num1 + num2);
                     dp[i].add(num1 - num2);
-                    dp[i].add(num1 * num2);
                     if(num2 !== 0){
                         dp[i].add(Math.floor(num1 / num2));
                     }
@@ -17,9 +18,5 @@ function solution(N, number) {
         }
         if(dp[i].has(number)) return i;
     }
-    
-    // for(const e of dp){
-    //     console.log(e);
-    // }
     return -1;
 }
