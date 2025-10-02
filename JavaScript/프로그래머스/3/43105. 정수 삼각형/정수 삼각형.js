699f871fb1d2c47b8fa68772e23b779131edb5bb
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-02 15:29:14 +0900

    [level 3] Title: 정수 삼각형, Time: 5.48 ms, Memory: 41.2 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ JavaScript/프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,20 +1,22 @@
 function solution(triangle) {
-    
     for(let i = 1; i < triangle.length; i++){
-        for(let j = 0 ; j < triangle[i].length; j++){
-            if(j === 0){ // 맨 왼쪽
-                triangle[i][j] += triangle[i-1][j]; 
-            } else if (j === triangle[i].length - 1) { // 맨 오른쪽
-                triangle[i][j] += triangle[i-1][j-1]; 
-            } else { // 가운데
-                triangle[i][j] += Math.max(triangle[i-1][j-1], triangle[i-1][j]);
+        for(let j = 0; j < triangle[i].length; j++){
+            if(j === 0){ // 맨 왼쪽이면
+                triangle[i][j] += triangle[i-1][j];
+            } else if ( j === triangle[i].length - 1) { // 맨 오른쪽이면
+                triangle[i][j] += triangle[i-1][triangle[i-1].length - 1];
+            } else { // 둘다아니면
+                triangle[i][j] += Math.max(triangle[i-1][j] , triangle[i-1][j-1]);
             }
         }
     }
-    let maxValue = 0;
-    for(let i = 0 ; i < triangle[triangle.length - 1].length; i++){
-        maxValue = Math.max(maxValue, triangle[triangle.length-1][i]);
+    // for(const e of triangle){
+    //     console.log(e);
+    //     console.log("-------------");
+    // }
+    let max = 0;
+    for(const e of triangle[triangle.length-1]){
+        if( e > max ) max = e;
     }
-    
-    return maxValue;
+    return max;
 }
