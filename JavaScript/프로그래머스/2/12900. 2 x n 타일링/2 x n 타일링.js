98e48b93701b45d22520d8f428e8574fb5378109
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-03 15:47:09 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.50 ms, Memory: 36.3 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -0,0 +1,19 @@
+function solution(n) {
+    let a = 1;
+    let b = 2;
+    if( n === 1 ){
+        return 1;
+    }
+    if( n === 2){
+        return 2;
+    }
+    
+    while(n > 3){
+        const result = (a + b) % 1000000007;
+        a = b;
+        b = result;
+        n--;
+    }
+    
+    return (a + b) % 1000000007;
+}
