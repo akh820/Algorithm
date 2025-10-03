006a298ec3692c376c35b7295c4f9ce3a7a03590
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-03 13:15:54 +0900

    [level 3] Title: 등굣길, Time: 2.51 ms, Memory: 33.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/42898. 등굣길/등굣길.js
+++ JavaScript/프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,25 +1,32 @@
 function solution(m, n, puddles) {
-    const number = 1000000007
-    const dp = Array.from({length : n + 1}, () => Array( m + 1).fill(0));
-    // xy 뒤집혀있는거 자각할것 m = y, n = x;
+    const dp = Array.from({length : n + 1}, () => Array(m+1).fill(0));
+    
+    dp[1][1] = 1;
+    
     for(const [x,y] of puddles){
         dp[y][x] = -1;
     }
-    dp[1][1] = 1
-    // m(x) = 4 , n(y) = 3
+    //m => y, n => x
+    const num = 1000000007
     for(let y = 1; y <= n; y++){
         for(let x = 1; x <= m; x++){
             if(dp[y][x] === -1){
                 dp[y][x] = 0;
                 continue;
-            } 
+            }
+            
             if( x > 1 ){
-                dp[y][x] = (dp[y][x] + dp[y][x-1]) % number;
+                dp[y][x] = (dp[y][x-1] + dp[y-1][x]) % num;
             }
             if( y > 1 ){
-                dp[y][x] = (dp[y][x] + dp[y-1][x]) % number;
+                dp[y][x] = (dp[y-1][x] + dp[y][x-1]) % num;
             }
         }
     }
+    
+    // for(const e of dp){
+    //     console.log(e);
+    //     console.log("-------------");
+    // }
     return dp[n][m];
 }
