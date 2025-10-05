0d98970cf83c487d83a4e6559e32e9935e97b753
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-05 15:30:19 +0900

    [level 3] Title: 입국심사, Time: 878.02 ms, Memory: 43.6 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/3/43238. 입국심사/입국심사.js
@@ -0,0 +1,23 @@
+function solution(n, times) {
+    const nBigInt = BigInt(n);
+    
+    let left = 1n;
+    let right = nBigInt * BigInt(Math.max(...times));
+    let answer = right;
+    
+    while(left <= right){
+        const mid = (left + right) / 2n;
+        let count = 0n;
+        for(const time of times){
+            count += mid / BigInt(time);
+        }
+        if(count >= n){
+            answer = mid;
+            right = mid - 1n;
+        } else {
+            left = mid + 1n;
+        }
+    }
+    
+    return BigInt(answer);
+}
