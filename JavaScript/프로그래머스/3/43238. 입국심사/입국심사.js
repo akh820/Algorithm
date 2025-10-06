1a2f5d3dba4eca61f81dcf05fe094c3d7a7fa224
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-06 13:15:03 +0900

    [level 3] Title: 입국심사, Time: 712.59 ms, Memory: 43.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/43238. 입국심사/입국심사.js
+++ JavaScript/프로그래머스/3/43238. 입국심사/입국심사.js
@@ -1,17 +1,20 @@
 function solution(n, times) {
-    const nBigInt = BigInt(n);
+    const nBigInt = BigInt(n); // big
     
-    let left = 1n;
-    let right = nBigInt * BigInt(Math.max(...times));
-    let answer = right;
+    let left = 1n; //기다리는사람 최소1, 걸리는 시간 1분
+    let right = nBigInt * BigInt(Math.max(...times)); // n * 최대시간 '
+    
+    let answer = right; // 최악의 경우를 가정
     
     while(left <= right){
-        const mid = (left + right) / 2n;
         let count = 0n;
+        const mid = (left + right) / 2n;
+        
         for(const time of times){
             count += mid / BigInt(time);
         }
-        if(count >= n){
+        
+        if(count >= nBigInt){ // n 보다 크거나 같은 경우 더 줄일 수 있다고 생각해서 오른쪽 범위를 줄임
             answer = mid;
             right = mid - 1n;
         } else {
