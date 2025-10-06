e3b43f5b1897d1305bb7baa85acc886aaeee47d9
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-06 13:28:02 +0900

    [level 2] Title: 조이스틱, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42860. 조이스틱/조이스틱.js
+++ JavaScript/프로그래머스/2/42860. 조이스틱/조이스틱.js
@@ -1,26 +1,29 @@
 function solution(name) {
     
-    function upDown(alphabet){
-        const code = alphabet.charCodeAt(0);
-        return Math.min(91 - code, code - 65);
-    }
-    
     let upDownCount = 0;
+    
     for(const e of name){
         upDownCount += upDown(e);
     }
-    let smallestCount = name.length - 1;
+    
+    let leftRightCount = name.length - 1; //일렬로 가는것을 최소값으로 계산
     
     for(let i = 0 ; i < name.length; i++){
-        let nextIndex = i + 1;
-        while(nextIndex < name.length && name[nextIndex] === "A"){
-            nextIndex++;
-        }
-        const goRightThenTurnLeft = (i * 2) + (name.length - nextIndex);
-        const goLeftThenTurnRight = (name.length - nextIndex) * 2 + i;
+        let currentIndex = i + 1;
+        while(currentIndex < name.length && name[currentIndex] === "A"){
+            currentIndex++;
+        } // 건너 띄기
         
-        smallestCount = Math.min(smallestCount, goRightThenTurnLeft, goLeftThenTurnRight);
+        const goRightThenTurnLeft = i * 2 + (name.length - currentIndex);
+        const goLeftThenTurnRight = (name.length - currentIndex) * 2 + i;
+        
+        leftRightCount = Math.min(goRightThenTurnLeft, goLeftThenTurnRight, leftRightCount);
+        
     }
     
-    return smallestCount + upDownCount;
+    function upDown(alphabet){
+        const a = alphabet.charCodeAt(0);
+        return Math.min( a - 65, 91 - a);
+    }
+    return upDownCount + leftRightCount;
 }
