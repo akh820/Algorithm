83babfe7587a56cae62328b106a0cdd312d77452
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-07 15:59:30 +0900

    [level 3] Title: 가장 먼 노드, Time: 45.99 ms, Memory: 56.2 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -0,0 +1,27 @@
+function solution(n, edge) {
+    
+    const connectedList = Array.from({length : n + 1} , () => []);
+    for(const [a,b] of edge){
+        connectedList[a].push(b);
+        connectedList[b].push(a);
+    }
+    const distances = new Array(n + 1).fill(-1);
+    
+    const queue = [1];
+    distances[1] = 0;
+    
+    while(queue.length > 0){
+        const currentNode = queue.shift();
+        
+        for(const neighbor of connectedList[currentNode]){ // [3, 2]
+            if(distances[neighbor] === -1){
+                distances[neighbor] = distances[currentNode] + 1;
+                queue.push(neighbor);
+            }
+        }
+    }
+    
+    const maxDistance = Math.max(...distances);
+
+    return distances.filter( e => e === maxDistance ).length;
+}
