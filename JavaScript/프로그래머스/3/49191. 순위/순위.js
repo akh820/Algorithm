e1df90f3c938f71af7b57abfb4317dae7611b831
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-09 19:50:45 +0900

    [level 3] Title: 순위, Time: 28.33 ms, Memory: 37.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/49191. 순위/순위.js
+++ JavaScript/프로그래머스/3/49191. 순위/순위.js
@@ -1,32 +1,31 @@
 function solution(n, results) {
-    const graph = Array.from({length:n+1}, () => Array(n + 1).fill(false));
+    const graph = Array.from({length: n +1}, () => Array(n+1).fill(false));
     
-    for(const e of results){
-        const [winner, loser] = e;
-        graph[winner][loser] = true; 
+    for(const [winner, loser] of results){
+        graph[winner][loser] = true;
     }
     
-    for(let i = 1; i <= n; i++){
-        for(let j = 1; j <= n; j++){
-            for(let k = 1; k <= n ; k++){
-                if(graph[j][i] && graph[i][k]){
-                    graph[j][k] = true;
+    for(let k = 1; k <= n; k++){
+        for(let i = 1; i <= n; i++){
+            for(let j = 1; j <= n; j++){
+                if(graph[i][k] && graph[k][j]){
+                    graph[i][j] = true;
                 }
             }
-        }
+        }   
     }
+    
     let answer = 0;
-    for(let i = 1 ; i <= n; i++){
+    for(let i = 1; i <= n; i++){
         let count = 0;
-        for(let j = 1; j <=n ;j++){
-            if(graph[i][j] || graph [j][i]){
+        for(let j = 1; j <=n; j++){
+            if(graph[i][j] || graph[j][i]){
                 count++;
             }
         }
-        if(count === n-1){
+        if(count === n - 1){
             answer++;
         }
     }
-    
     return answer;
 }
