b8609e4df05ac1e9d530a4c3b2431f6f5571d1cd
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-09 15:56:56 +0900

    [level 3] Title: 가장 먼 노드, Time: 62.88 ms, Memory: 56.2 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ JavaScript/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,26 +1,27 @@
 function solution(n, edge) {
-    // 양방향 노드가 있는 리스트 
-    // 최소 길이를 저장하는 맵
-    // DFS방식
-    const connectedList = Array.from({length: n + 1}, ()=>[]);
     
+    const connectedList = Array.from({length : n + 1} , () => []);
     for(const [a,b] of edge){
         connectedList[a].push(b);
         connectedList[b].push(a);
     }
-    const distanceList = Array(n+1).fill(-1);
+    const distances = new Array(n + 1).fill(-1);
     
-    const queue = [1]; // 초기노드 1에서 시작
-    distanceList[1] = 0;
+    const queue = [1];
+    distances[1] = 0;
+    
     while(queue.length > 0){
-        const node = queue.shift(); // node 1 에서 시작
-        for(const side of connectedList[node]){
-            if(distanceList[side] === -1){
-                distanceList[side] = distanceList[node] + 1;
-                queue.push(side);
+        const currentNode = queue.shift();
+        
+        for(const neighbor of connectedList[currentNode]){ // [3, 2]
+            if(distances[neighbor] === -1){
+                distances[neighbor] = distances[currentNode] + 1;
+                queue.push(neighbor);
             }
         }
     }
-    const max = Math.max(...distanceList)
-    return distanceList.filter( e => e === max).length;
+    
+    const maxDistance = Math.max(...distances);
+
+    return distances.filter( e => e === maxDistance ).length;
 }
