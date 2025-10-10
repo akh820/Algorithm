f1b2b033de658d20f99f0f2027f86ad66e215ec7
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-10 21:15:29 +0900

    [level 3] Title: 징검다리 건너기, Time: 59.49 ms, Memory: 43.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
+++ JavaScript/프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
@@ -2,26 +2,26 @@ function solution(stones, k) {
     
     let left = 1;
     let right = 200000000;
-    let answer = 0;
+    
+    let answer = 0; // 그냥 초깃값
     
     while(left <= right){
         const mid = Math.floor((left + right) / 2);
         
         let zeroCount = 0;
         let canCross = true;
-        
         for(const stone of stones){
             if(stone - mid < 0){
                 zeroCount++;
             } else {
                 zeroCount = 0;
             }
-            
-            if(zeroCount >= k){
+            if(zeroCount === k){
                 canCross = false;
                 break;
             }
         }
+        
         
         if(canCross){
             answer = mid;
@@ -30,5 +30,6 @@ function solution(stones, k) {
             right = mid - 1;
         }
     }
-    return answer;
+    
+    return answer
 }
