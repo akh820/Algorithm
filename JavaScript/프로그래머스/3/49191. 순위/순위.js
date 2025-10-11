d8517e19d4bd9c15e899b1d049c76f6b271e681b
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-11 13:03:13 +0900

    [level 3] Title: 순위, Time: 281.66 ms, Memory: 37.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/49191. 순위/순위.js
+++ JavaScript/프로그래머스/3/49191. 순위/순위.js
@@ -1,25 +1,24 @@
 function solution(n, results) {
-    const graph = Array.from({length: n +1}, () => Array(n+1).fill(false));
+    const player = Array.from({length:n + 1}, () => Array(n+1).fill(false));
     
     for(const [winner, loser] of results){
-        graph[winner][loser] = true;
+        player[winner][loser] = true;
     }
     
-    for(let k = 1; k <= n; k++){
-        for(let i = 1; i <= n; i++){
-            for(let j = 1; j <= n; j++){
-                if(graph[i][k] && graph[k][j]){
-                    graph[i][j] = true;
+    for(let i = 1 ; i <= n; i++){
+        for(let j = 1; j <=n; j++){
+            for(let k = 1; k <=n; k++){ //i는 1 k는 2, j = 5라고 가정
+                if(player[j][i] && player[i][k]){
+                    player[j][k] = true;
                 }
             }
-        }   
+        }
     }
-    
     let answer = 0;
     for(let i = 1; i <= n; i++){
-        let count = 0;
-        for(let j = 1; j <=n; j++){
-            if(graph[i][j] || graph[j][i]){
+    let count = 0;
+        for(let j = 1; j <= n ; j++){
+            if(player[i][j] || player[j][i]){
                 count++;
             }
         }
