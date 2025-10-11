eab7e2a4ec54c78dd91b4cfe13dd8c607e596cf1
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-11 19:14:08 +0900

    [level 3] Title: 합승 택시 요금, Time: 85.16 ms, Memory: 44.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
+++ JavaScript/프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
@@ -1,29 +1,25 @@
 function solution(n, s, a, b, fares) {
-    const dist = Array.from({length: n+1}, () => Array(n+1).fill(Infinity));
+    const root = Array.from({length: n+1} , () => Array(n+1).fill(Infinity));
+    for(let i = 1 ; i <= n ; i++){
+        root[i][i] = 0;
+    }
+    for(const [a,b, fare] of fares){
+        root[a][b] = fare;
+        root[b][a] = fare;
+    }
     
-    for(let i = 1 ; i <= n; i++){
-        dist[i][i] = 0;
-    }
-    for(const [c,d,fare] of fares){
-        dist[c][d] = fare;
-        dist[d][c] = fare;
-    }
-    // for(const e of dist){
-    //     console.log(e);
-    //     console.log("------------------");
-    // }
-    for(let k = 1 ; k <= n; k++){ // 경유 노드
-        for(let i = 1 ; i <= n ; i++){// 시작 노드
-            for(let j = 1; j <=n ; j++){// 도착지 노드
-                dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+    for(let i = 1; i <=n ; i++){
+        for(let j = 1; j <=n ; j++){
+            for(let k = 1; k <=n ; k++){
+                root[j][k] = Math.min(root[j][k], root[j][i] + root[i][k]);
             }
-        }
+        }    
     }
-    let min_fare = Infinity;
-    for(let k = 1; k <= n ; k++){
-        if(min_fare > (dist[s][k] + dist[k][a] + dist[k][b])){
-            min_fare = dist[s][k] + dist[k][a] + dist[k][b];
-        }
+    let minFare = root[s][a]+root[s][b];
+    
+    for(let i = 1; i <= n; i++){
+        const sharedFare = root[s][i] + root[i][a] + root[i][b];
+        minFare = Math.min(sharedFare, minFare);
     }
-    return min_fare;
+    return minFare;
 }
