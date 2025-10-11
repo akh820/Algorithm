9869cbbae9934198ea33b805dafa532294730972
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-11 13:30:46 +0900

    [level 3] Title: 등굣길, Time: 1.32 ms, Memory: 33.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/42898. 등굣길/등굣길.js
+++ JavaScript/프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,32 +1,23 @@
 function solution(m, n, puddles) {
-    const dp = Array.from({length : n + 1}, () => Array(m+1).fill(0));
-    
-    dp[1][1] = 1;
-    
-    for(const [x,y] of puddles){
-        dp[y][x] = -1;
+    const road = Array.from({length: n + 1}, () => Array(m+1).fill(0)) ;
+    // road[1][2] = 1; // y,x
+    for(const [x, y] of puddles){
+        road[y][x] = -1;
     }
-    //m => y, n => x
-    const num = 1000000007
-    for(let y = 1; y <= n; y++){
+    road[1][1] = 1;
+    for(let y = 1; y <= n ; y++){
         for(let x = 1; x <= m; x++){
-            if(dp[y][x] === -1){
-                dp[y][x] = 0;
+            if(road[y][x] === -1){
+                road[y][x] = 0;
                 continue;
             }
-            
-            if( x > 1 ){
-                dp[y][x] = (dp[y][x-1] + dp[y-1][x]) % num;
+            if( y > 1){
+                road[y][x] = (road[y][x] + road[y-1][x]) % 1000000007;
             }
-            if( y > 1 ){
-                dp[y][x] = (dp[y-1][x] + dp[y][x-1]) % num;
+            if( x > 1){
+                road[y][x] = (road[y][x] + road[y][x-1]) % 1000000007;
             }
         }
     }
-    
-    // for(const e of dp){
-    //     console.log(e);
-    //     console.log("-------------");
-    // }
-    return dp[n][m];
+    return road[n][m];
 }
