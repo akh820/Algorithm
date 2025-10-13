987aa7022adde855997dd584c8f6849d0a9b4282
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-13 12:23:19 +0900

    [level 3] Title: 가장 먼 노드, Time: 70.51 ms, Memory: 56.2 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ JavaScript/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,27 +1,22 @@
 function solution(n, edge) {
-    
-    const connectedList = Array.from({length : n + 1} , () => []);
+    let nodeList = Array.from({length: n + 1}, () => []);
     for(const [a,b] of edge){
-        connectedList[a].push(b);
-        connectedList[b].push(a);
+        nodeList[a].push(b);
+        nodeList[b].push(a);
     }
-    const distances = new Array(n + 1).fill(-1);
-    
     const queue = [1];
-    distances[1] = 0;
     
+    const minDistance = Array(n+1).fill(-1);
+    minDistance[1] = 0;
     while(queue.length > 0){
-        const currentNode = queue.shift();
-        
-        for(const neighbor of connectedList[currentNode]){ // [3, 2]
-            if(distances[neighbor] === -1){
-                distances[neighbor] = distances[currentNode] + 1;
-                queue.push(neighbor);
+        const currentIdx = queue.shift();
+        for(const e of nodeList[currentIdx]){
+            if(minDistance[e] === -1){
+                minDistance[e] = minDistance[currentIdx] + 1;
+                queue.push(e);
             }
         }
     }
-    
-    const maxDistance = Math.max(...distances);
-
-    return distances.filter( e => e === maxDistance ).length;
+    const max = Math.max(...minDistance);
+    return minDistance.filter(e => e === max).length;
 }
