e6ed6bff0642fa8185238690826ee63d5d2d9d8a
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-14 16:32:33 +0900

    [level 3] Title: 등굣길, Time: 1.40 ms, Memory: 33 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/42898. 등굣길/등굣길.js
+++ JavaScript/프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,23 +1,24 @@
 function solution(m, n, puddles) {
-    const road = Array.from({length: n + 1}, () => Array(m+1).fill(0)) ;
-    // road[1][2] = 1; // y,x
-    for(const [x, y] of puddles){
-        road[y][x] = -1;
+    const constant = 1000000007;
+    const rootMap = Array.from({length:n+1}, () => Array(m+1).fill(0));
+    //m => y , n => x
+    for(const [x,y] of puddles){
+        rootMap[y][x] = -1;
     }
-    road[1][1] = 1;
-    for(let y = 1; y <= n ; y++){
+    rootMap[1][1] = 1;
+    for(let y = 1 ; y <= n; y++){
         for(let x = 1; x <= m; x++){
-            if(road[y][x] === -1){
-                road[y][x] = 0;
+            if(rootMap[y][x] === -1){
+                rootMap[y][x] = 0;
                 continue;
             }
-            if( y > 1){
-                road[y][x] = (road[y][x] + road[y-1][x]) % 1000000007;
+            if(x > 1){
+                rootMap[y][x] += (rootMap[y][x-1] % constant);
             }
-            if( x > 1){
-                road[y][x] = (road[y][x] + road[y][x-1]) % 1000000007;
+            if(y > 1){
+                rootMap[y][x] += (rootMap[y-1][x] % constant);
             }
         }
     }
-    return road[n][m];
+    return rootMap[n][m] % constant;
 }
