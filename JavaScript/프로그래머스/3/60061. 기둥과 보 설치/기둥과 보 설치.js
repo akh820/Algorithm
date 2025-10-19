84bd636b7af7ef1371710a1368af5a2a212cd101
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-19 15:02:21 +0900

    [level 3] Title: 기둥과 보 설치, Time: 83.27 ms, Memory: 41.2 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.js
+++ JavaScript/프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.js
@@ -2,38 +2,7 @@ function solution(n, build_frame) {
     
     const set = new Set();
     
-    for(const [x,y,a,b] of build_frame){
-        const order = `${x},${y},${a}`;
-        if(b === 1){//설치하는 경우
-            set.add(order); // 일단 설치
-            if(!isValidate(order)){ // 유효하지 않을 경우 삭제
-                set.delete(order)
-            }
-        } else {
-            set.delete(order);
-            
-            let isDangerous = false;
-            for(const e of set){
-                if(!isValidate(e)){
-                    isDangerous = true;
-                    break;
-                } 
-            }
-            if(isDangerous){
-                set.add(order);
-            }
-        }
-    }
-    
-    const list = Array.from(set).map(e=>e.split(",").map(Number));
-
-    return list.sort((a,b) => {
-        if(a[0] !== b[0]) return a[0] - b[0];
-        if(a[1] !== b[1]) return a[1] - b[1];
-        return a[2] - b[2]
-    })
-    
-    function isValidate(order) {
+    const isValidate = (order) => {
         const [x,y,a] = order.split(",").map(Number);
         if(a === 0){ // 기둥일시
             if(y === 0) return true;
@@ -46,4 +15,39 @@ function solution(n, build_frame) {
             return false;
         }
     }
+    
+    for(const [x, y, a, b] of build_frame){
+        // a가 0 기둥, 1 보, 
+        // b가 0 삭제, 1 설치
+        const order = `${x},${y},${a}`;
+        if(b === 1){
+            set.add(order);
+            
+            if(!isValidate(order)){
+                set.delete(order);
+            }
+        } else {
+            set.delete(order);
+            
+            let isDangerous = false;
+            for(const e of set){
+                if(!isValidate(e)){
+                    isDangerous = true;
+                    break;
+                }
+            }
+            if(isDangerous){
+                set.add(order);
+            }
+        }
+    }
+    const list = Array.from(set).map(e => e.split(",").map(Number));
+    
+    list.sort((a,b) => {
+        if(a[0] !== b[0]) return a[0] - b[0];
+        if(a[1] !== b[1]) return a[1] - b[1];
+        return a[2] - b[2]
+    })
+    
+    return list;
 }
