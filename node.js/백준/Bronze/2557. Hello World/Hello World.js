cf9536af600694402459059f3d23614ff0519b49
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-20 17:58:01 +0900

    [Bronze V] Title: Hello World, Time: 80 ms, Memory: 9332 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/2557. Hello World/Hello World.js
@@ -0,0 +1,5 @@
+// 백준 표준 템플릿
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// 로컬 테스트: const input = fs.readFileSync('input.txt')...
+console.log("Hello World!");
