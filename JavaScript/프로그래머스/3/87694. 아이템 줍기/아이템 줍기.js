f3d433fdfd57118722c991c61878078868af869f
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-23 00:10:25 +0900

    [level 3] Title: 아이템 줍기, Time: 2.74 ms, Memory: 33.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/87694. 아이템 줍기/아이템 줍기.js
+++ JavaScript/프로그래머스/3/87694. 아이템 줍기/아이템 줍기.js
@@ -1,54 +1,60 @@
 function solution(rectangle, characterX, characterY, itemX, itemY) {
     
-    const double = rectangle.map(([x1,y1,x2,y2]) => [x1*2,y1*2,x2*2,y2*2]);
+    const recs = rectangle.map(([x1,y1,x2,y2]) => [x1 * 2,y1* 2,x2* 2,y2* 2]);
     
-    const aSet = new Set();
+    const set1 = new Set(); // 테두리 전부를 
     
-    for(const [x1,y1,x2,y2] of double){
+    for(const [x1,y1,x2,y2] of recs){
         for(let i = x1; i <= x2; i++){
-            aSet.add(`${i},${y1}`);
-            aSet.add(`${i},${y2}`);
+            set1.add(`${i},${y1}`);
+            set1.add(`${i},${y2}`);
         }
         for(let i = y1; i <= y2; i++){
-            aSet.add(`${x1},${i}`);
-            aSet.add(`${x2},${i}`);
+            set1.add(`${x1},${i}`);
+            set1.add(`${x2},${i}`);
         }
     }
-    const bMap = new Map();
     
-    for(const e of aSet){
-        const [x, y] = e.split(",").map(Number);
-        let isIn = false;
-        for(const [x1,y1,x2,y2] of double){
+    const set2 = new Set(); // 테두리 전부 중에서 안에있는 테두리 걷어냄
+    
+    for(const e of set1){// e = "1,3,2,4"
+        const [x,y] = e.split(",").map(Number);
+        
+        let isInBoundary = false;
+        
+        for(const [x1,y1,x2,y2] of recs){
             if(x > x1 && x < x2 && y > y1 && y < y2){
-                isIn = true;
+                isInBoundary = true;
                 break;
             }
         }
-        if(!(isIn === true)){
-            bMap.set(`${x},${y}`, false);
+        if(!isInBoundary){
+            set2.add(`${x},${y}`);
         }
     }
     
+    const isVisited = new Map();
+    for(const e of set2){
+        isVisited.set(e, false);
+    }
+    isVisited.set(`${characterX *2},${characterY*2}`, true);
+    
+    let dx = [-1,1,0,0];
+    let dy = [0,0,1,-1];
+    
     const queue = [[characterX*2, characterY*2, 0]];
-    
-    bMap.set(`${characterX*2},${characterY*2}`, true);
-    
-    const dx = [-1,1,0,0];
-    const dy = [0,0,-1,1];
     
     while(queue.length > 0){
         const [x, y, distance] = queue.shift();
-        if(x === itemX*2 && y === itemY*2){
+        if(x === itemX * 2 && y === itemY * 2){
             return distance / 2;
         }
         
         for(let i = 0 ; i < 4; i++){
             const nx = dx[i] + x;
             const ny = dy[i] + y;
-            const temp = `${nx},${ny}`;
-            if(bMap.has(temp) && bMap.get(temp) === false){
-                bMap.set(temp,true);
+            if(isVisited.get(`${nx},${ny}`) === false){
+                isVisited.set(`${nx},${ny}`,true);
                 queue.push([nx, ny, distance + 1]);
             }
         }
