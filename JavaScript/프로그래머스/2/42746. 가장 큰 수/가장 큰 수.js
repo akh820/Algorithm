4cd6f1abbe3e16194c5c20342d16ce25bcb0e4df
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-24 15:06:23 +0900

    [level 2] Title: 가장 큰 수, Time: 159.95 ms, Memory: 44.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ JavaScript/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,6 +1,5 @@
 function solution(numbers) {
-    const list = numbers.map(String).sort((a,b) => (b+a) - (a+b));
-    // b + a = "610", a + b = "106"; 504 양수 바꿈
-    // b + a = "102", a + b = "210"; -109 음수 안바꿈
-    return list[0] === "0" ? "0" : list.join("");
+    const temp = numbers.map(String).sort((a,b) => (b + a) - (a + b))
+    
+    return temp[0] === "0" ? "0" : temp.join("");
 }
