8058dc22236f53d1bb1c2769fd5e533e0994e09e
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-25 15:51:32 +0900

    [level 3] Title: 합승 택시 요금, Time: 74.72 ms, Memory: 37.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
+++ JavaScript/프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
@@ -1,33 +1,23 @@
 function solution(n, s, a, b, fares) {
-    const nodes = Array.from({length: n + 1}, () => Array(n+1).fill(Infinity));
-    
-    for(const [start,end,fare] of fares){
-        nodes[start][end] = fare;
-        nodes[end][start] = fare;
-    }
-    for(let i = 1 ; i <= n ; i++){
+    const nodes = Array.from({length: n + 1 } , () => Array(n+1).fill(Infinity));
+    for(let i = 0 ; i <= n ; i++){
         nodes[i][i] = 0;
     }
-    
-    for(let i = 1 ; i <= n ; i++){
-        for(let j = 1 ; j <= n ; j++){
-            for(let k = 1 ; k <= n ; k++){
+    for(const [a, b, fare] of fares){
+        nodes[a][b] = fare;
+        nodes[b][a] = fare;
+    }
+    for(let i = 1; i <= n ; i++){
+        for(let j = 1; j <= n ; j++){
+            for(let k = 1; k <= n ; k++){
                 nodes[j][k] = Math.min(nodes[j][k], nodes[j][i] + nodes[i][k]);
             }
         }    
     }
-    
     let minFare = nodes[s][a] + nodes[s][b];
-    
-    for(let i = 1; i <= n; i++){
-        for(let j = 1; j <=n ; j++){
-            const shared = nodes[s][i] + nodes[i][a] + nodes[i][b];
-            minFare = Math.min(minFare, shared);
-        }
+    for(let i = 1 ; i <= n ; i++){
+        const sharedFare = nodes[s][i] + nodes[i][a] + nodes[i][b];
+        minFare = Math.min(minFare, sharedFare)
     }
-
-    // for(const e of nodes){
-    //     console.log(e.toString());
-    // }
     return minFare;
 }
