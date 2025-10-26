4f1395c59cb21cfa5cbd22923ed4f7e227c4da9b
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-26 14:14:50 +0900

    [Silver II] Title: 스택 수열, Time: 216 ms, Memory: 33212 KB -BaekjoonHub

--- node.js/백준/Silver/1874. 스택 수열/스택 수열.js
+++ node.js/백준/Silver/1874. 스택 수열/스택 수열.js
@@ -3,28 +3,16 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const N = Number(input[0]); //8 개수가져와서 변환 맞고
 
-let list = 1; // 수열 넣을꺼 빼놓고
+let index = 1; 
 const stack = [];
 const print = []; // 출력할꺼 빼놓고
 for(let i = 1 ; i <= N ; i++){
-    const num = Number(input[i]); 
-    while(true){ // 1 <= 8
-        if(stack.length !== 0 && (num === stack[stack.length-1])){
-            stack.pop();
-            print.push("-");
-            break;
-        } else {
-            if(list > N) break;
-            stack.push(list);
-            list++;
-            print.push("+");
-        }
+    stack.push(i);
+    print.push("+");
+    while(stack.length > 0 && stack[stack.length - 1] == input[index]){
+        stack.pop();
+        print.push("-");
+        index++;
     }
 }
-if(stack.length === 0){
-    for(const e of print){
-        console.log(e);
-    }
-} else {
-    console.log("NO");
-}
+console.log(stack.length === 0 ? print.join("\n"):"NO");
