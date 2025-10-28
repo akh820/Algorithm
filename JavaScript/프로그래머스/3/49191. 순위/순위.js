791aab1e1d2b6dd5c01ebaa040b9244958168ae6
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-28 14:35:47 +0900

    [level 3] Title: 순위, Time: 9.83 ms, Memory: 37.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/49191. 순위/순위.js
+++ JavaScript/프로그래머스/3/49191. 순위/순위.js
@@ -1,25 +1,22 @@
 function solution(n, results) {
-    
-    const players = Array.from({length:n+1}, () => Array(n+1).fill(false));
-    
+    const players = Array.from({length:n + 1} , () => Array(n+1).fill(false));
     for(const [winner, loser] of results){
-        players[winner][loser] = true; // 여기서 true는 순위를 알 수 있음을 뜻함.
+        players[winner][loser] = true;
     }
     
-    for(let i = 0 ; i <= n ; i++){
-        for(let j = 0 ; j <= n ; j++){
-            for(let k = 0 ; k <= n ; k++){
-                if(players[j][i] && players[i][k]){
+    for(let i = 1; i <= n ; i++){
+        for(let j = 1; j <= n ; j++){
+            for(let k = 1; k <= n ; k++){
+                if(players[j][i] === true && players[i][k] === true){
                     players[j][k] = true;
                 }
             }
         }   
     }
-    let answer = 0 ;
-    
-    for(let i = 0 ; i <= n ; i++){
+    let answer = 0;
+    for(let i = 1 ; i <=n ; i++){
         let count = 0;
-        for(let j = 0 ; j <= n ; j++){
+        for(let j = 1; j <=n ; j++){
             if(players[i][j] || players[j][i]){
                 count++;
             }
@@ -28,8 +25,8 @@ function solution(n, results) {
             answer++;
         }
     }
-    
-    // players.forEach((e,i) => console.log(i,e.toString()));
-    
+    // for(const e of players){
+    //     console.log(e.toString());
+    // }
     return answer;
 }
