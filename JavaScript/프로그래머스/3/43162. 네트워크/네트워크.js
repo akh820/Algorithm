5b09c592ef2f48db4c5ca18cb7dcd618e5fb847b
안계홍 <122023428+akh820@users.noreply.github.com>
2025-10-28 15:49:09 +0900

    [level 3] Title: 네트워크, Time: 0.41 ms, Memory: 33.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/43162. 네트워크/네트워크.js
+++ JavaScript/프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,22 +1,23 @@
 function solution(n, computers) {
-    // 1. 모든 노드를 다 돌아야함
-    const visited = new Array(n).fill(false);
-    let network = 0;
-    for(let i = 0 ; i < n; i++){
-        if(visited[i] === false){
-            //방문된적 없으면 새로운 네트워크
-            network++;
-            dfs(i);
+    
+    const isConnected = Array(n).fill(false);
+    let networkCount = 0;
+    
+    for(let i = 0 ; i < n; i ++){
+        if(isConnected[i] === false){
+            networkCount++; // 1
+            dfs(i)
         }
     }
-    // 2. 연결된놈부터 다시 dfs
+    
     function dfs(startNode){
-        visited[startNode] = true;
-        for(let i = 0; i < n ; i++){
-            if(computers[startNode][i] === 1 && visited[i] === false){
+        isConnected[startNode] = true;
+        for(let i = 0 ; i < n; i++){
+            if(computers[startNode][i] === 1 && isConnected[i] === false){
                 dfs(i);
             }
         }
-    };
-    return network;
+    }
+    
+    return networkCount;
 }
