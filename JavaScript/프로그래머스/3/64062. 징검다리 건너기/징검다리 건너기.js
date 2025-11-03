bed0083ed863641ddf103f6ff4537c39afa21f97
안계홍 <122023428+akh820@users.noreply.github.com>
2025-11-03 16:54:21 +0900

    [level 3] Title: 징검다리 건너기, Time: 31.80 ms, Memory: 43.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
+++ JavaScript/프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
@@ -1,35 +1,35 @@
 function solution(stones, k) {
+    
+    // stones => 돌배열
+    // k = 한번에 건너띌 수 있는 최대 디딤돌 칸 수
     let left = 1;
     let right = 200000000;
-    
-    let answer = 1;
+    let answer = 0;
     
     while(left <= right){
         const mid = Math.floor((left + right) / 2);
-        
-        let zeroCount = 0;
+        // console.log(mid);
+        let continueCount = 0;
         let canCross = true;
-        
         for(const stone of stones){
-            if(stone - mid < 0){
-                zeroCount++;
+        // -1,1,2,0,-1,-2,1,-1,2,-2
+            if(stone - mid <= 0){
+                continueCount++;
             } else {
-                zeroCount = 0;
+                continueCount = 0;
             }
             
-            if(zeroCount === k){
+            if(continueCount === k){
                 canCross = false;
                 break;
             }
         }
-        
-        if(canCross){
+        if(!canCross){
             answer = mid;
-            left = mid + 1;
-        } else {
             right = mid - 1;
+        } else {
+            left = mid + 1;
         }
     }
-    
     return answer;
 }
