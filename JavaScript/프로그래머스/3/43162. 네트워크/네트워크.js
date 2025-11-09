7eec7da882b35f700dc52066beead3686d0cb5ce
안계홍 <122023428+akh820@users.noreply.github.com>
2025-11-09 14:57:07 +0900

    [level 3] Title: 네트워크, Time: 0.45 ms, Memory: 33.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/43162. 네트워크/네트워크.js
+++ JavaScript/프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,23 +1,21 @@
 function solution(n, computers) {
     
-    const N = computers.length;
-    
     const visited = Array(n).fill(false);
+    
     let networkCount = 0;
     
-    for(let i = 0 ; i < N ; i++){
-        if(visited[i] === false){
-            networkCount++;
-            dfs(i);
-        }
-    }
-    
-    function dfs(startNode){
+    const dfs = (startNode) => {
         visited[startNode] = true;
-        for(let i = 0 ; i < computers[startNode].length; i++){
-            if(computers[startNode][i] === 1 && visited[i] === false){
+        for(let i = 0 ; i < computers[startNode].length ; i++){
+            if(!visited[i] && computers[startNode][i] === 1){
                 dfs(i);
             }
+        }
+    }
+    for(let i = 0 ; i < n ; i ++){
+        if(!visited[i]){
+            networkCount++;
+            dfs(i);
         }
     }
     
