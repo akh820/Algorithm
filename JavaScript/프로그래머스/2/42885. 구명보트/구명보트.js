c0ec3eca30ccb99805bd16d9ae00039287764704
안계홍 <122023428+akh820@users.noreply.github.com>
2025-11-10 15:27:54 +0900

    [level 2] Title: 구명보트, Time: 14.20 ms, Memory: 38.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42885. 구명보트/구명보트.js
+++ JavaScript/프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,12 +1,11 @@
 function solution(people, limit) {
-    let boat = 0;
-    
     let left = 0;
     let right = people.length - 1;
     
+    let boat = 0;
+    
     people.sort((a,b) => a - b);
     
-    //50 50 50 70 80
     while(left <= right){
         if(people[left] + people[right] <= limit){
             left++;
@@ -15,7 +14,6 @@ function solution(people, limit) {
             right--;
         }
         boat++;
-    }
-    
+    };
     return boat;
 }
