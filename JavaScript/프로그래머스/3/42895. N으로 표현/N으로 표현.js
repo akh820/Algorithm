00464c6335f95ef0b58986ffcc6014e58b802649
안계홍 <122023428+akh820@users.noreply.github.com>
2025-11-16 00:33:53 +0900

    [level 3] Title: N으로 표현, Time: 38.72 ms, Memory: 39.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/42895. N으로 표현/N으로 표현.js
+++ JavaScript/프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -1,21 +1,22 @@
 function solution(N, number) {
-    const nums = Array.from({length: 9}, () => new Set());
-    for(let i = 1 ; i < 9 ; i++){
-        nums[i].add(Number(String(N).repeat(i)));
-        for(let j = 1 ; j < i; j++){
+    const set = Array.from({length:9}, () => new Set());
+    
+    for(let i = 0 ; i < 9; i++){
+        set[i].add(Number(String(N).repeat(i)));
+        for(let j = 0 ; j < i; j++){
             const k = i - j;
-            for(const num1 of nums[k]){
-                for(const num2 of nums[j]){
-                    nums[i].add(num1 + num2);
-                    nums[i].add(num1 - num2);
-                    nums[i].add(num1 * num2);
+            for(const num1 of set[j]){
+                for(const num2 of set[k]){
+                    set[i].add(num1 + num2);
+                    set[i].add(num1 - num2);
+                    set[i].add(num1 * num2);
                     if(num2 !== 0){
-                        nums[i].add(Math.floor(num1 / num2));
+                        set[i].add(Math.floor(num1 / num2));
                     }
                 }
             }
         }
-        if(nums[i].has(number)) return i;
+        if(set[i].has(number)) return i;
     }
     return -1;
 }
