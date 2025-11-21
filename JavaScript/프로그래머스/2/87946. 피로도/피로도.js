df675bb62a0e4a1d71e9ce10663d68d1e365453e
안계홍 <122023428+akh820@users.noreply.github.com>
2025-11-21 14:32:40 +0900

    [level 2] Title: 피로도, Time: 37.62 ms, Memory: 38 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/87946. 피로도/피로도.js
+++ JavaScript/프로그래머스/2/87946. 피로도/피로도.js
@@ -1,18 +1,19 @@
 function solution(k, dungeons) {
-        
-    const N = dungeons.length;
+    /*
+    방문했는지 안했는지 체크 리스트
+    */
     
-    const isVisited = Array(N).fill(false);
+    const isVisited = Array(dungeons.length).fill(false);
     
-    let max = 0;
+    let maxCount = 0;
     
-    dfs(k, 0);
+    dfs(k,0);
     
     function dfs(currentFatigue, clearCount){
-        max = Math.max(max, clearCount);
-        for(let i = 0 ; i < N ; i++){
+        maxCount = Math.max(clearCount, maxCount);
+        for(let i = 0 ; i < dungeons.length; i++){
             const [required, usedFatigue] = dungeons[i];
-            if(isVisited[i] === false && currentFatigue >= required){
+            if(currentFatigue >= required && isVisited[i] === false){
                 isVisited[i] = true;
                 dfs(currentFatigue - usedFatigue, clearCount + 1);
                 isVisited[i] = false;
@@ -20,5 +21,5 @@ function solution(k, dungeons) {
         }
     }
     
-    return max;
+    return maxCount;
 }
